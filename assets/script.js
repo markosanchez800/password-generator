417dd37da739ebc299bc961a4f2d9e4df7f35780
komar zashcen <markosanchez800@gmail.com>
2021-03-25 11:42:04 -0700

    Added arrays containing all possible character values

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+uppers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+numz = ["0","1","2","3","4","5","6","7","8","9"];
+downers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
+specials = ["!","@","#","$","%","^","&","*","(",")","-","_","+","=","`","~","[","{","]","}","|",";",":","'",",","<",".",">","?","/"];
 
-function generatePassword(){
+function generatePassword(){ 
+  var charLim = window.prompt("How many characters should the password be 8-128")
+  if (charLim < 8 || charLim > 128){
+   alert("Start over and select a number within correct bounds");
+   return;
+  }
   var specChar = window.confirm("Special Characters?")
+  var lilChar = window.confirm("Lowercase letters?")
   var capChar = window.confirm("Capital letters?")
   var numChar = window.confirm("Numbers?")
-  var charLim = window.prompt("How many characters should the password be 8-128")
+  getPassword();
 }
-
 
 
 // Write password to the #password input
